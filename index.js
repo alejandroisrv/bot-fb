608906b09aeae93a04a713046717a9232513378c
Alejandro Sojo <alejandrojsrv@gmail.com>
2023-08-25 20:56:32 -0400

    Chat bot

--- index.js
+++ index.js
@@ -1,28 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request')
 
 const app = express().use(bodyParser.json());
 
-app.post('/webhook', (req, res) =>{
-    console.log('POST: webhook');
+const PAGE_ACCESS_TOKEN = "EAADkyZBtVhZBABOZCYo82EIWz3erbj5nqZCGIDx7AScLPocBKk4s0CndkrTpWGJlGfMVNMQvsvuw6iZCmZAZAoDXl2Eq37PXhukjJ0cfjp8BZAHUPUZBDa4vCO6U4cvDOP5iZCKcOUvDvg7mZAfcdAHt4aYytJ6ta3ocZBxbC6ZBjp2PCZCP0P8Y3jJyvylSEV9vZBtrPETyUOAmjy9qZBwsreMI5EfKQIGL4qyH9hLGdsYNGlHk7b0ZD"
 
+function sendMessengerResponse(senderPsid, response) {
+    // Construir el cuerpo de la solicitud
+    const requestBody = {
+        recipient: {
+            id: senderPsid
+        },
+        message: response
+    };
+
+    // Enviar la solicitud a la API de Messenger
+    request({
+        uri: 'https://graph.facebook.com/v17.0/me/messages',
+        qs: { access_token: PAGE_ACCESS_TOKEN },
+        method: 'POST',
+        json: requestBody
+    }, (err, res, body) => {
+        if (!err && res.statusCode === 200) {
+            console.log('Mensaje enviado a Facebook Messenger');
+        } else {
+            console.error('No se pudo enviar el mensaje a Facebook Messenger');
+            console.error(res);
+            console.error(body);
+        }
+    });
+}
+
+app.post('/webhook', (req, res) => {
     const body = req.body;
 
-    if(body.object === 'page'){
-    
+    if (body.object === 'page') {
         body.entry.forEach(entry => {
             // se reciben y procesan los mensajes
             const webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
+            // Verificar si el evento es un mensaje de texto
+            if (webhookEvent.message && webhookEvent.message.text) {
+                const messageText = webhookEvent.message.text;
+
+                // Construir la respuesta
+                const response = {
+                    text: `Gracias por tu mensaje: "${messageText}"`
+                };
+
+                // Enviar la respuesta a Facebook Messenger
+                sendMessengerResponse(senderPsid, response);
+            }
         });
 
         res.status(200).send('EVENTO RECIBIDO');
-    }else{
+    } else {
         res.sendStatus(404);
     }
 });
 
-app.get('/webhook', (req, res) =>{
+app.get('/webhook', (req, res) => {
     console.log('GET: webhook');
 
     const VERIFY_TOKEN = 'stringUnicoParaTuAplicacion';
@@ -31,18 +68,18 @@ app.get('/webhook', (req, res) =>{
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    if(mode && token){
-        if(mode === 'subscribe' && token === VERIFY_TOKEN){
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
             console.log('WEBHOOK VERIFICADO');
             res.status(200).send(challenge);
-        }else{
+        } else {
             res.sendStatus(404);
         }
-    }else{
+    } else {
         res.sendStatus(404);
     }
 });
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Servidor iniciado...');
 });

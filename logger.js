9683c3b13f787eb8a5a66c0ce7c09c726844c88a
Alejandro Sojo <alejandrojsrv@gmail.com>
2023-08-27 19:59:54 -0400

    log de envento recibido

--- logger.js
+++ logger.js
@@ -1,13 +1,34 @@
-const winston = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
 
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'my-app' },
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' })
+        new transports.Console({
+            format: combine(
+                timestamp(),
+                myFormat
+            ),
+            level: 'error'
+        }),
+        new transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            format: combine(
+                timestamp(),
+                myFormat
+            ),
+            maxsize: 5242880, // 5MB
+            maxFiles: 5
+        })
     ]
 });
 
-module.exports = logger
+module.exports = logger;
